abff249abc79e6c6459ea8b451496550a51a734e
Leonardo Leitão <leoleitao27@gmail.comgit config --global user.email leoleitao27@gmail.comgit config --global user.email leoleitao27@gmail.com>
2022-11-26 19:19:00 -0300

    encapsulamento

---
+++ POO/0002 - Encapsulamento/POO Mensagem/Mensagem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_Mensagem
+{
+    internal class Mensagem
+    {
+        public string TextoMensagem;
+
+
+        public void exibirMensage()
+        {
+            Console.WriteLine(this.TextoMensagem);
+        }
+    }
+}
---
+++ POO/0002 - Encapsulamento/POO Mensagem/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_Mensagem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mensagem msg1;
+            msg1 = new Mensagem();
+            msg1.TextoMensagem = "Olá Mundo!";
+
+            msg1.exibirMensage();
+
+            Console.ReadKey();
+
+        }
+    }
+}
